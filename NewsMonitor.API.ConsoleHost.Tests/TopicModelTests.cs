c871c5db81b62d668bbd6d5c30880ae0fe5a7e9b
malabarMCB <grinevich97@gmail.com>
2017-03-29 10:52:06 +0300

    Add Topic model, controller and tests

---
+++ NewsMonitor.API.ConsoleHost.Tests/TopicModelTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using Ninject;
+using Xunit;
+using NewsMonitor.API.ConsoleHost.Models;
+using Newtonsoft.Json;
+
+namespace NewsMonitor.API.ConsoleHost.Tests
+{
+    public class TopicModelTests
+    {
+        private readonly TestServer _server;
+
+        public TopicModelTests()
+        {
+            _server = TestServer.Create(app => OwinStartup.CoreConfiguration(app, PrepareContainer));
+        }
+
+        private StandardKernel PrepareContainer()
+        {
+            var kernel = new StandardKernel();
+
+            return kernel;
+        }
+
+        [Fact]
+        public async void GetCurrentTopic()
+        {
+            using (var client = _server.HttpClient)
+            {
+                var response = await client.GetAsync("topics/1");
+
+                // assert
+                response.IsSuccessStatusCode.Should().BeTrue();
+                var message = await response.Content.ReadAsStringAsync();
+
+                var jsonTopic =JsonConvert.DeserializeObject<TopicModel>(message);
+
+                jsonTopic.ShouldBeEquivalentTo(new TopicModel
+                { 
+                    Date = Convert.ToDateTime("2017-02-12"),
+                    Description = "topic 1.......",
+                    Keyword = "key1",
+                    Title = "Topic 1"
+                });
+            }
+        }
+
+        [Fact]
+        public async void GetOutOfRangeTopic()
+        {
+            using (var client = _server.HttpClient)
+            {
+                var response = await client.GetAsync("topics/2");
+
+                // assert
+                response.IsSuccessStatusCode.Should().BeTrue();
+                var message = await response.Content.ReadAsStringAsync();
+
+                var jsonTopic = JsonConvert.DeserializeObject<TopicModel>(message);
+
+                jsonTopic.ShouldBeEquivalentTo(new TopicModel());
+            }
+        }
+    }
+}
---
+++ NewsMonitor.API.ConsoleHost/Models/ArticleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsMonitor.API.ConsoleHost.Models
+{
+    class ArticleModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Keyword { get; set; }
+
+        public string Text { get; set; }
+    }
+}
